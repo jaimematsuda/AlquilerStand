38b14ed0532dc9e2af783909480fa85e2be9e041
jaimematsuda <jaimematsuda@gmail.com>
2016-08-15 11:22:54 -0500

    modificaciones 01 para levantar en heroku

--- Alquiler/settings.py
+++ Alquiler/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'Alquiler.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=500) 
 
- DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 #DATABASES = {
 #    'default': {
