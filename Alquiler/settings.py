2407320b0dad58d65d1b53dd36fd464e3a0a906b
jaimematsuda <jaimematsuda@gmail.com>
2016-08-14 20:20:47 -0500

    configuracion de DATABASES de DJANGO

--- Alquiler/settings.py
+++ Alquiler/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,6 +89,7 @@ WSGI_APPLICATION = 'Alquiler.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES['default'] =  dj_database_url.config()
 
 DATABASES = {
     'default': {
